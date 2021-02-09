4e33e571ad53cb7165ad0e166d33404b1147d950
marco bottoni <bottoni.mrc@gmail.com>
2021-02-09 16:35:04 +0100

    cars 2.0

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\CarModel;
 
 class CarController extends Controller
 {
         public function index()
     {
+        $cars = CarModel::all();
+        dd($cars);
         return view('cars');
     }
 }
